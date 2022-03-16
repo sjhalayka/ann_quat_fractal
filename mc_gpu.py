1d7e37498167c4a3700be21453ffcb370ffea74c
Shawn Halayka <sjhalayka@gmail.com>
2022-03-16 10:07:12 -0600

    Add files via upload

--- mc_gpu.py
+++ mc_gpu.py
@@ -2,7 +2,6 @@ import numpy as np
 import math
 import torch
 import os.path
-import time
 
 from numpy import random
 from skimage import measure
@@ -10,7 +9,7 @@ from torch.autograd import Variable
 from os import path
 
 device = "cpu"#torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-print("using device: " + str(device))
+print(device)
 
 
 
@@ -34,7 +33,6 @@ class quaternion:
 
     def __str__(self): 
         return str(self.x) + ", " + str(self.y) + ", " + str(self.z) + ", " + str(self.w);
-
 
 
 
@@ -75,16 +73,13 @@ Z.w = z_w;
 
 
 
-
-float_slice_a = torch.zeros((res* res, num_components), dtype=torch.float32)#.to(device);
-float_slice_b = torch.zeros((res* res, num_components), dtype=torch.float32)#.to(device);
+float_slice = torch.zeros((res* res, 2*num_components), dtype=torch.float32);
 float_array = np.zeros((res, res, res), dtype = np.float32);
 
 
-def get_predictions():
-    batch = torch.cat((float_slice_a, float_slice_b), 1)
-    #batch.to(device)
-    return net(batch).detach().numpy();
+
+
+
 
 
 
@@ -107,7 +102,7 @@ class Net(torch.nn.Module):
 
 
 
-net = Net()#.to(device)
+net = Net().to(device)
 
 if path.exists('weights_4_100000.pth'):
     net.load_state_dict(torch.load('weights_4_100000.pth'))
@@ -120,23 +115,9 @@ else:
 
 
 
-t0= time.perf_counter()
 
 
 
-
-x_steps = torch.linspace(x_grid_min, x_grid_max, steps=x_res);
-y_steps = torch.linspace(y_grid_min, y_grid_max, steps=y_res);
-z_steps = torch.linspace(z_grid_min, z_grid_max, steps=z_res);
-
-#loc = np.zeros((res, res, res), dtype = quaternion);
-
-loc = np.zeros((res * res * res), dtype = quaternion);
-
-for z in range(res):#z_step in z_steps:
-    for x in range(res):#x_step in x_steps:
-        for y in range(res):#y_step in y_steps:
-            loc[z*res*res + y*res + x] = quaternion(x_steps[x], y_steps[y], z_steps[z], z_w);
 
 
 
@@ -146,32 +127,30 @@ for i in range(z_res):
 
     print(str(i))
 
+    Z.x = x_grid_min;
+
     print("init")
     
     for j in range(x_res):
 
+        Z.y = y_grid_min;
+
         for k in range(y_res):
         
-            index = j*res + k;
+            index = j*res + k
 
-            Z = loc[i*res*res + k*res + j];# = quaternion(x_steps[x], y_steps[y], z_steps[z], z_w);
+            float_slice[index][0] = Z.x;
+            float_slice[index][1] = Z.y;
+            float_slice[index][2] = Z.z;
+            float_slice[index][3] = Z.w;
+            float_slice[index][4] = Z.x;
+            float_slice[index][5] = Z.y;
+            float_slice[index][6] = Z.z;
+            float_slice[index][7] = Z.w;
 
-            float_slice_a[index][0] = Z.x;
-            float_slice_a[index][1] = Z.y;
-            float_slice_a[index][2] = Z.z;
-            float_slice_a[index][3] = Z.w;
-            float_slice_b[index][0] = Z.x;
-            float_slice_b[index][1] = Z.y;
-            float_slice_b[index][2] = Z.z;
-            float_slice_b[index][3] = Z.w;
+            Z.y += y_step_size;
 
-        #    Z.y += y_step_size;
-
-       # Z.x += x_step_size;
-   
-    #float_slice_a.to(device);
-    #float_slice_b.to(device);
-
+        Z.x += x_step_size;
 
     print("done init")
 
@@ -180,13 +159,14 @@ for i in range(z_res):
 
     for m in range(max_iterations):
 
-#        print(m);
+        print(m);
 
-        p = get_predictions();
+        p = net(float_slice).detach().numpy();
 
         Z.x = x_grid_min;
 
         for j in range(x_res):
+
             Z.y = y_grid_min;
 
             for k in range(y_res):
@@ -199,25 +179,25 @@ for i in range(z_res):
                 p[index][3] += C.w;
 
                 float_slice_magnitude[j][k] = math.sqrt(p[index][0]*p[index][0] + p[index][1]*p[index][1] + p[index][2]*p[index][2] + p[index][3]*p[index][3]);
-                float_slice_a[index][0] = float_slice_b[index][0] = torch.from_numpy(p)[index][0];
-                float_slice_a[index][1] = float_slice_b[index][1] = torch.from_numpy(p)[index][1];
-                float_slice_a[index][2] = float_slice_b[index][2] = torch.from_numpy(p)[index][2];
-                float_slice_a[index][3] = float_slice_b[index][3] = torch.from_numpy(p)[index][3];
-
+                
+                float_slice[index][0] = torch.from_numpy(p)[index][0];
+                float_slice[index][1] = torch.from_numpy(p)[index][1];
+                float_slice[index][2] = torch.from_numpy(p)[index][2];
+                float_slice[index][3] = torch.from_numpy(p)[index][3];
+                float_slice[index][4] = torch.from_numpy(p)[index][0];
+                float_slice[index][5] = torch.from_numpy(p)[index][1];
+                float_slice[index][6] = torch.from_numpy(p)[index][2];
+                float_slice[index][7] = torch.from_numpy(p)[index][3];
+  
                 Z.y += y_step_size;
 
             Z.x += x_step_size;
 
-        #float_slice_a.to(device);
-        #float_slice_b.to(device);
-
     Z.z += z_step_size;
 
     float_array[i] = float_slice_magnitude;
-"""
-t1 = time.perf_counter()
 
-print("Time elapsed: ", t1 - t0)
+
 
 
 verts, faces, normals, values = measure.marching_cubes(float_array, threshold)
