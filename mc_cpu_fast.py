b10350b55979496a5b8179722b559733d1f45f6b
Shawn Halayka <sjhalayka@gmail.com>
2022-03-18 11:16:54 -0600

    Add files via upload

---
+++ mc_cpu_fast.py
@@ -0,0 +1,187 @@
+import numpy as np
+import math
+import torch
+import os.path
+import time
+import numba
+
+from numpy import random
+from skimage import measure
+from torch.autograd import Variable
+from os import path
+
+
+device = "cpu"#torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print(device)
+
+
+num_components = 4; # quaternions
+
+
+
+
+@numba.njit
+def calc_slice(float_slice, grid_min, res, Z_x, Z_y, Z_z, Z_w, C_x, C_y, C_z, C_w, step_size, p, float_array, i):
+
+    Z_x = grid_min;
+
+    for j in range(res):
+
+       Z_y = grid_min;
+
+       for k in range(res):
+
+           index = j*res + k
+
+           p[index][0] += C_x;
+           p[index][1] += C_y;
+           p[index][2] += C_z;
+           p[index][3] += C_w;
+
+           float_array[i][j][k] = math.sqrt(p[index][0]*p[index][0] + p[index][1]*p[index][1] + p[index][2]*p[index][2] + p[index][3]*p[index][3]);
+   
+           float_slice[index][0] = p[index][0];
+           float_slice[index][1] = p[index][1];
+           float_slice[index][2] = p[index][2];
+           float_slice[index][3] = p[index][3];
+           float_slice[index][4] = p[index][0];
+           float_slice[index][5] = p[index][1];
+           float_slice[index][6] = p[index][2];
+           float_slice[index][7] = p[index][3];
+  
+           Z_y += step_size;
+
+       Z_x += step_size;
+
+
+
+@numba.njit
+def init_slice(float_slice, grid_min, res, Z_x, Z_y, Z_z, Z_w, step_size):
+
+    Z_x = grid_min;
+
+    for j in range(res):
+
+        Z_y = grid_min;
+
+        for k in range(res):
+        
+            index = j*res + k;
+
+            float_slice[index][0] = Z_x;
+            float_slice[index][1] = Z_y;
+            float_slice[index][2] = Z_z;
+            float_slice[index][3] = Z_w;
+            float_slice[index][4] = Z_x;
+            float_slice[index][5] = Z_y;
+            float_slice[index][6] = Z_z;
+            float_slice[index][7] = Z_w;
+
+            Z_y += step_size;
+
+        Z_x += step_size;
+
+
+@numba.njit
+def calc_muls_slice(float_slice, res):
+
+    p = np.zeros((res * res, num_components), dtype=np.float64);
+
+    for j in range(res):
+
+       for k in range(res):
+
+           index = j*res + k
+
+           left_x = right_x = float_slice[index][0];
+           left_y = right_y = float_slice[index][1];
+           left_z = right_z = float_slice[index][2];
+           left_w = right_w = float_slice[index][3];
+
+           p[index][0] = left_x*right_x - left_y*right_y - left_z*right_z - left_w*right_w;
+           p[index][1] = left_x*right_y + left_y*right_x + left_z*right_w - left_w*right_z;
+           p[index][1] = left_x*right_z - left_y*right_w + left_z*right_x + left_w*right_y;
+           p[index][3] = left_x*right_w + left_y*right_z - left_z*right_y + left_w*right_x;
+
+    return p;
+
+
+
+
+
+def main():
+
+    res = 1000;
+    grid_max = 1.5;
+    grid_min = -grid_max;
+    
+    z_w = 0;
+
+    #C = quaternion();
+    C_x = 0.3;
+    C_y = 0.5;
+    C_z = 0.4;
+    C_w = 0.2;
+
+    max_iterations = 8;
+    threshold = 4.0;
+    
+    step_size = (grid_max - grid_min) / (res - 1);
+
+    #Z = quaternion();
+    Z_x = grid_min;
+    Z_y = grid_min;
+    Z_z = grid_min;
+    Z_w = z_w;
+
+    float_slice = np.zeros((res* res, 2*num_components), dtype=np.float64);
+    float_array = np.zeros((res, res, res), dtype = np.float32);
+
+    t0 = time.perf_counter()
+
+    for i in range(res):
+
+        print(str(i))      
+        print("init")
+        init_slice(float_slice, grid_min, res, Z_x, Z_y, Z_z, Z_w, step_size);
+        print("done init")
+
+        for m in range(max_iterations):
+
+            print(m);
+
+            p = calc_muls_slice(float_slice, res);
+
+            calc_slice(float_slice, grid_min, res, Z_x, Z_y, Z_z, Z_w, C_x, C_y, C_z, C_w, step_size, p, float_array, i);
+
+        Z_z += step_size;
+
+
+
+    t1 = time.perf_counter()
+
+    print("Time elapsed: ", t1 - t0)
+
+
+
+    verts, faces, normals, values = measure.marching_cubes(float_array, threshold, spacing=(step_size, step_size, step_size))
+
+    thefile = open('test_ai.obj', 'w')
+    for item in verts:
+      thefile.write("v {0} {1} {2}\n".format(item[0], item[1], item[2]))
+
+    for item in normals:
+      thefile.write("vn {0} {1} {2}\n".format(item[0], item[1], item[2]))
+
+    for item in faces:
+      thefile.write("f {0}//{0} {1}//{1} {2}//{2}\n".format(item[0] + 1, item[1] + 1, item[2] + 1))  
+
+    thefile.close()
+
+
+
+
+
+
+main();
+
---
+++ mc_cpu_slow.py
@@ -0,0 +1,152 @@
+import numpy as np
+import math
+import os.path
+import time
+
+from numpy import random
+from skimage import measure
+
+from os import path
+
+
+
+num_components = 4 # quaternions
+
+
+class quaternion:
+    x = 0;
+    y = 0;
+    z = 0;
+    w = 0;
+
+    def __str__(self): 
+        return str(self.x) + ", " + str(self.y) + ", " + str(self.z) + ", " + str(self.w);
+
+def quat_mul(left, right):
+    ret = quaternion();
+
+    ret.x = left.x*right.x - left.y*right.y - left.z*right.z - left.w*right.w;
+    ret.y = left.x*right.y + left.y*right.x + left.z*right.w - left.w*right.z;
+    ret.z = left.x*right.z - left.y*right.w + left.z*right.x + left.w*right.y;
+    ret.w = left.x*right.w + left.y*right.z - left.z*right.y + left.w*right.x;
+
+    return ret;
+
+
+def quat_add(left, right):
+    ret = quaternion();
+
+    ret.x = left.x + right.x;
+    ret.y = left.y + right.y;
+    ret.z = left.z + right.z;
+    ret.w = left.w + right.w;
+
+    return ret;
+
+
+
+def self_dot(q):
+    return q.x*q.x + q.y*q.y + q.z*q.z + q.w*q.w;
+    
+def magnitude(q):
+    return math.sqrt(self_dot(q));
+
+def iterate(Z):
+    for i in range(max_iterations):
+
+        Z = quat_mul(Z, Z);
+        Z = quat_add(Z, C);
+        
+        #if magnitude(Z) >= threshold:
+        #    break;
+    
+    return magnitude(Z);
+
+
+
+
+
+
+
+res = 1000;
+float_array = np.zeros((res, res, res), np.float32)
+
+x_grid_max = 1.5;
+y_grid_max = 1.5;
+z_grid_max = 1.5;
+x_grid_min = -x_grid_max;
+y_grid_min = -y_grid_max;
+z_grid_min = -z_grid_max;
+x_res = res;
+y_res = res;
+z_res = res;
+    
+z_w = 0;
+
+C = quaternion();
+C.x = 0.3;
+C.y = 0.5;
+C.z = 0.4;
+C.w = 0.2;
+
+max_iterations = 8;
+threshold = 4.0;
+    
+x_step_size = (x_grid_max - x_grid_min) / (x_res - 1);
+y_step_size = (y_grid_max - y_grid_min) / (y_res - 1);
+z_step_size = (z_grid_max - z_grid_min) / (z_res - 1);
+
+Z = quaternion();
+Z.x = x_grid_min;
+Z.y = y_grid_min;
+Z.z = z_grid_min;
+Z.w = z_w;
+
+
+t0 = time.perf_counter()
+
+
+for i in range(z_res):
+
+    Z.x = x_grid_min;
+
+    print(str(i))
+
+    for j in range(x_res):
+
+        Z.y = y_grid_min;
+
+        for k in range(y_res):    
+
+            float_array[i][j][k] = iterate(Z)
+
+            Z.y += y_step_size;
+
+        Z.x += x_step_size;
+
+    Z.z += z_step_size;
+
+
+
+
+t1 = time.perf_counter()
+
+print("Time elapsed: ", t1 - t0)
+
+
+verts, faces, normals, values = measure.marching_cubes(float_array, threshold, spacing=(x_step_size, y_step_size, z_step_size))
+
+thefile = open('test.obj', 'w')
+for item in verts:
+  thefile.write("v {0} {1} {2}\n".format(item[0], item[1], item[2]))
+
+for item in normals:
+  thefile.write("vn {0} {1} {2}\n".format(item[0], item[1], item[2]))
+
+for item in faces:
+  thefile.write("f {0}//{0} {1}//{1} {2}//{2}\n".format(item[0] + 1, item[1] + 1, item[2] + 1))  
+
+thefile.close()
+
+
+
